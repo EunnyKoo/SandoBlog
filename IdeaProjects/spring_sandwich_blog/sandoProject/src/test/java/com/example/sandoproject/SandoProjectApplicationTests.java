83e75839831cb9ebe16c4b0874721155390fb86e
EunnyKoo <139302489+EunnyKoo@users.noreply.github.com>
2023-12-01 12:13:32 +0900

    Add files via upload

---
+++ IdeaProjects/spring_sandwich_blog/sandoProject/src/test/java/com/example/sandoproject/SandoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sandoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
