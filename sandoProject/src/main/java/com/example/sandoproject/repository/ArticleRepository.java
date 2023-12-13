a1cbe61c0b1309be301d9e841728040046702a55
EunnyKoo <139302489+EunnyKoo@users.noreply.github.com>
2023-12-13 18:35:51 +0900

    Delete sandoProject directory

--- sandoProject/src/main/java/com/example/sandoproject/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sandoproject.repository;
-
-import com.example.sandoproject.entity.Article;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArticleRepository extends CrudRepository<Article, Long> {
-}
