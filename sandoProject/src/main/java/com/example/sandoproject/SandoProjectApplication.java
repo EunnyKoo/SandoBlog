b885822522565f6a1e4f7221e3b5300e9a55a5b3
EunnyKoo <139302489+EunnyKoo@users.noreply.github.com>
2023-12-16 12:23:21 +0900

    update: 리다이렉트 추가

---
+++ sandoProject/src/main/java/com/example/sandoproject/SandoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sandoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandoProjectApplication.class, args);
+    }
+
+}
