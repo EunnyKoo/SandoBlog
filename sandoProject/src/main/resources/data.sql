1beb15a6da9d34002aecf7149080fe936dc95c0b
EunnyKoo <139302489+EunnyKoo@users.noreply.github.com>
2023-12-18 15:28:30 +0900

    Delete sandoProject directory

--- sandoProject/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO article(id, title, content) VALUES(1, 'DAN', 'I LOVE EUN');
-INSERT INTO article(id, title, content) VALUES(2, 'EUN', 'I LOVE MYSELF');
-INSERT INTO article(id, title, content) VALUES(3, 'MJ', 'I LOVE MY LIL SIS');
-INSERT INTO article(id, title, content) VALUES(4, 'JIHUN', 'I LOVE SIS');
-INSERT INTO article(id, title, content) VALUES(5, 'LOVE', 'I LOVE YOU');
